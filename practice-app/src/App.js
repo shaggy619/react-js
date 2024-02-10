2c644b31ee78c9abc33f81612332d7340b79150a
shaggy <someaim1@gmail.com>
2024-02-10 09:23:34 +0545

    Passed entire object as props

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,46 +1,36 @@
 import "./App.css";
 
-const firstBook = {
-  img: "https://m.media-amazon.com/images/I/5112eIqM9pL._SY445_SX342_.jpg",
-  title: "The Women",
-  author: "Kristin Hannah",
-};
+const books = [
+  {
+    img: "https://m.media-amazon.com/images/I/5112eIqM9pL._SY445_SX342_.jpg",
+    title: "The Women",
+    author: "Kristin Hannah",
+    id: 1,
+  },
 
-const secondBook = {
-  img: "https://m.media-amazon.com/images/I/41C-012Es8L._SY445_SX342_.jpg",
-  title: "Atomic Habits",
-  author: "James Clear",
-};
+  {
+    img: "https://m.media-amazon.com/images/I/41C-012Es8L._SY445_SX342_.jpg",
+    title: "Atomic Habits",
+    author: "James Clear",
+    id: 2,
+  },
 
-const thirdBook = {
-  img: "https://m.media-amazon.com/images/I/41yzzcHjHCL._SY445_SX342_.jpg",
-  title: "Psychology of Money",
-  author: " Morgan House",
-};
+  {
+    img: "https://m.media-amazon.com/images/I/41yzzcHjHCL._SY445_SX342_.jpg",
+    title: "Psychology of Money",
+    author: " Morgan House",
+    id: 3,
+  },
+];
 
 function App() {
   return (
     <>
       <h1>Amazon Best Sellers</h1>
       <section className="book-list">
-        <Book
-          img={firstBook.img}
-          title={firstBook.title}
-          author={firstBook.author}
-        >
-          {/* <button>Click Me</button>
-          <p>Random Text</p> */}
-        </Book>
-        <Book
-          img={secondBook.img}
-          title={secondBook.title}
-          author={secondBook.author}
-        />
-        <Book
-          img={thirdBook.img}
-          title={thirdBook.title}
-          author={thirdBook.author}
-        />
+        {books.map((book) => {
+          return <Book {...book} key={book.id} />;
+        })}
       </section>
     </>
   );
