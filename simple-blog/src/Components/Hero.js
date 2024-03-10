450dedaac740977e0f30d442fa5967de7898e313
shaggy <someaim1@gmail.com>
2024-03-10 06:59:31 +0545

    Created Custom Hook useFetch

--- simple-blog/src/Components/Hero.js
+++ simple-blog/src/Components/Hero.js
@@ -1,12 +1,8 @@
-import React, { useEffect, useState } from "react";
 import Blogs from "./Blogs";
+import useFetch from "./useFetch";
 
 export default function Hero() {
-  const [blogs, setBlocks] = useState(null);
-
-  const [isLoading, setIsLoading] = useState(true);
-
-  const [error, setError] = useState(null);
+  const { data, isLoading, error } = useFetch("http://localhost:8000/blogs");
 
   // const handleDelete = (id) => {
   //   const newBlogs = blogs.filter((blog) => {
@@ -15,33 +11,13 @@ export default function Hero() {
   //   setBlocks(newBlogs);
   // };
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogss")
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("Failed to fetch resource!");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setBlocks(data);
-          setIsLoading(false);
-        })
-        .catch((err) => {
-          setError(err.message);
-          setIsLoading(false);
-        });
-    }, 1000);
-  }, []);
-
   return (
     <>
       <div className="main-content">
         {" "}
         {error && <div className="text-center warning">{error}</div>}
         {isLoading && <div className="text-center">Loading...</div>}
-        {blogs && <Blogs blogs={blogs} />}
+        {data && <Blogs blogs={data} />}
       </div>
     </>
   );
